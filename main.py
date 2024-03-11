2b08f6cb6e92f6a12e1177228234c10b5d19cb50
GAURAV DNYANESH MAHAJAN <108111718+Gaurav-22-11@users.noreply.github.com>
2024-03-11 12:02:27 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, HTTPException
+import json
+
+app=FastAPI()
+
+with open("data.json","r"):
+  data=json.load(f)
+
+@app.get("/")
+async def read_data():
+  return data
+
+@app.get("/{guid}")
+async def read_data_by_guid(guid: str):
+  for item in data:
+    if item["guid"]==guid
+      return item
+  raise HTTPException(status_code=404, detail="Item not Found")
+  
+
